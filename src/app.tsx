5b19a5800fb867bd9b87130533bd3d51556fea09
M Haidar Hanif <git@mhaidarhanif.com>
2025-11-03 22:46:29 +0700

    feat: profile props

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,14 @@ export function App() {
   return (
     <div>
       <h1>Task Management</h1>
-      <Profile />
+      <Profile
+        imageUrl="https://i.imgur.com/MK3eW3Am.jpg"
+        name="Katherine Johnson"
+      />
+      <Profile
+        imageUrl="https://thediffpodcast.com/assets/images/jordan-2c43ff762e625d49ea58424e3e74188a.jpg"
+        name="Jordan Walke"
+      />
     </div>
   );
 }
