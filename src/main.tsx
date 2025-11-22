99f1ca06edb85b822fbd558cbc09946ad5a257d6
M Haidar Hanif <git@mhaidarhanif.com>
2025-11-22 14:44:31 +0700

    feat: setup routes, root route with app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 import "./index.css";
 import { App } from "./app";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
   </BrowserRouter>,
 );
